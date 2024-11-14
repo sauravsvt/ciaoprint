1dd530e86a5055518fb4b4b7e11a396042353394
Saurav Shriwastav <sauravshriwastavaa@gmail.com>
2024-11-14 11:14:37 +0100

    added simpleanalytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      {/* Add the Simple Analytics script */}
+      <Script 
+        src="https://scripts.simpleanalyticscdn.com/latest.js" 
+        strategy="afterInteractive" 
+      />
+      
+      
+      {/* Render your page components */}
+      <Component {...pageProps} />
+    </>
+  )
 }
